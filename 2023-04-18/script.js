5e5b49b32f96118ef7964fb9dc0bf503fe29da5b
Mateus-Scarin <mateus.scarin@gmail.com>
2023-04-23 16:52:58 -0300

    feat: imagens de erros e função de mensagem

--- 2023-04-18/script.js
+++ 2023-04-18/script.js
@@ -1,21 +1,23 @@
 let errors = document.getElementById('cont-errors');
+let imgErrors = document.getElementById('img-errors');
 let letterTest = document.getElementById('letter-test');
+let informedLetters = document.getElementById('informed-letters');
 let word = document.getElementById('word');
+let inputMatch = document.getElementById('input-match');
 
-const words = ['EITA', 'TESTE', 'SCARIN'];
+const words = ['SCARIN', 'JAVASCRIPT', 'JAVA', 'RUBY', 'PYTHON', 'PHP', 'SWIFT', 'DART', 'KOTLIN', 'TYPESCRIPT'];
 let contErros = Number(0);
 let constructWord = [];
 let letters = [];
 let selectedWord;
 
-selectedWord = words[Math.floor(Math.random() * 3)];
+selectedWord = words[Math.floor(Math.random() * 10)];
 console.log(selectedWord);
 errors.innerHTML = `Erros: ${contErros}`
 for (let i = 0; i < selectedWord.length; i++) {
     word.innerHTML += '_ '
 }
 
-/* Confere se a letra informada existe na palavra a ser descoberta ou não */
 function letterMatch() {
     if (validateLetter()) {
         let controlError = true;
@@ -28,15 +30,17 @@ function letterMatch() {
         if (controlError) {
             contErros++;
             errors.innerHTML = `Erros: ${contErros}`;
+            imgErrors.innerHTML = `<img src="img/img${contErros}.png" alt="Imagem de erros">`;
         }
-        letters.push(letterTest.value);
+        letters.push(letterTest.value.toUpperCase());
         makeWord();
+        messageNotification();
+        informedLetters.innerHTML = `Letras já informadas: ${letters}`;
     } else {
         alert("Letra já foi informada!");
     }
 }
 
-/* Monta a palavra depois que começa os chutes */
 function makeWord() {
     for (let i = 0; i < selectedWord.length; i++) {
         if (selectedWord.charAt(i).toLowerCase() === letterTest.value.toLowerCase()) {
@@ -48,12 +52,11 @@ function makeWord() {
         }
     }
     word.innerHTML = ``;
-    for(let i = 0; i < constructWord.length; i++){
+    for (let i = 0; i < constructWord.length; i++) {
         word.innerHTML += `${constructWord[i]}`;
     }
 }
 
-/* Verifica se a letra informada já não foi chutada */
 function validateLetter() {
     for (let i = 0; i < letters.length; i++) {
         if (letters[i].toLowerCase() === letterTest.value.toLowerCase()) {
@@ -61,4 +64,26 @@ function validateLetter() {
         }
     }
     return true;
+}
+
+function messageNotification() {
+    if (contErros < 6) {
+        let controlMessage = true;
+        constructWord.forEach((character) => {
+            if (character == '_ ') {
+                controlMessage = false;
+            }
+        });
+        if (controlMessage) {
+            inputMatch.innerHTML = `<a href="jogo-da-forca.html">
+                                        <button>Jogar novamente!</button>
+                                    </a>`;
+            alert('Você ganhou!');
+        }
+    } else {
+        inputMatch.innerHTML = `<a href="jogo-da-forca.html">
+                                        <button>Jogar novamente!</button>
+                                    </a>`;
+        alert('Você perdeu!');
+    }
 }
