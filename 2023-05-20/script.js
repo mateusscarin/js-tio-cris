9271b672f3d5be61493eb3e1c30ceff3feda16ee
Mateus-Scarin <mateus.scarin@gmail.com>
2023-06-04 17:29:01 -0300

    feat: iniciando aprendizado com event listener

---
+++ 2023-05-20/script.js
@@ -0,0 +1,7 @@
+addEventListener("mousemove", function(event){
+    let dot = document.createElement("div");
+    dot.className = "dot";
+    dot.style.left = (event.pageX - 4) + "px";
+    dot.style.top = (event.pageY - 4) + "px";
+    document.body.appendChild(dot);
+});
